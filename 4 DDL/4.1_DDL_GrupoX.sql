677af28444e4f41bf958edccabcc8a51117b0ceb
DavidCalvoRivas <72581284+DavidCalvoRivas@users.noreply.github.com>
2020-10-19 01:00:44 +0200

    Update 4.1_DDL_GrupoX.sql

--- 4 DDL/4.1_DDL_GrupoX.sql
+++ 4 DDL/4.1_DDL_GrupoX.sql
@@ -16,7 +16,18 @@ name
 description
 category
 address
-
+    create table FACILITIES(
+    id number,
+    guid varchar2(1000),
+    name varchar2(1000) not null,
+    description varchar2(1000),
+    category varchar2(1000),
+    addres varchar2(1000),
+    constraint uq_fac_id primary key(id),
+    constraint uq_fac_name unique(name),
+    constraint uq_fac_guid unique(guid)
+    );  
+Create table 
 FLOORS
 id
 guid
@@ -25,7 +36,17 @@ category
 description
 height
 facilityId
-
+    create table FLOORS(
+    id number,
+    guid varchar2(1000),
+    name varchar2(1000) not null,
+    category varchar2(1000),
+    description varchar2(1000),
+    height number,
+    facilityId number,
+    constraint pk_flo_id primary key(id),
+    constraint uq_flo_name unique(name),
+    constraint uq_flo_guid unique(guid));
 SPACES
 id
 guid
@@ -35,7 +56,19 @@ description
 usableHeight
 area
 floorId
-
+    create table SPACES(
+    id number,
+    guid varchar2(1000),
+    name varchar2(1000) not null,
+    category varchar2(1000),
+    description varchar2(1000),
+    usableHeight number,
+    area number,
+    floorId number,
+    constraint pk_spa_id primary key(id),
+    constraint uq_spa_guid unique(guid),
+    constraint uq_spa_floorId unique(floorId),
+    constraint uq_spa_name unique(name));
 COMPONENTS
 id
 guid
@@ -45,7 +78,21 @@ serialNumber
 installatedOn
 spaceId
 typeId
-
+    create table COMPONENTS(
+    id number,
+    guid varchar2(1000),
+    name varchar2(1000) not null,
+    description varchar2(1000),
+    serialNumber number,
+    installatedOn date default sysdate,
+    spaceId number,
+    typeId number,
+    constraint pk_com_id primary key(id),
+    constraint uq_com_guid unique(guid),
+    constraint uq_com_serialNumber unique(serialNumber),
+    constraint uq_com_spaceId unique(spaceId),
+    constraint uq_com_typeId unique(typeId),
+    constraint uq_com_name unique(name));
 TYPES
 id
 guid
@@ -54,7 +101,17 @@ description
 modelNumber
 color
 warrantyYears
-
+    create table TYPES(
+    id number,
+    guid varchar2(1000),
+    name varchar2(1000) not null,
+    description varchar2(1000),
+    modelnumber number,
+    color varchar2(1000),
+    warrantyYears number,
+    constraint pk_typ_id primary key(id),
+    constraint uq_typ_name unique(name),
+    constraint uq_typ_guid unique(guid));
 
 En las definiciones establacer las siguientes restricciones
 -Los guid deben ser únicos.
